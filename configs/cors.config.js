78d7569ba19099bdac96e0e41d694aa826e5f7a0
santiagoaldanondo <santiagoaldanondo@gmail.com>
2017-10-02 00:41:58 +0200

    changed cors for heroku

--- configs/cors.config.js
+++ configs/cors.config.js
@@ -1,10 +1,11 @@
 // Configuration options: https://www.npmjs.com/package/cors#configuration-options
 
-const originsAllowed = process.env.CORS_ORIGIN;
+const originsAllowed = 'https://third-proyect-web.herokuapp.com'
 
 module.exports = {
     origin: function (origin, cb) {
         const allowed = originsAllowed.indexOf(origin) !== -1;
+        console.log(allowed)
         cb(null, allowed);
     },
     credentials: true,
